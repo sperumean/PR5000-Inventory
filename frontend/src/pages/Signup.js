7760b196d34272262e0ecbabc7590864ccedd5ba
sperumean <158227842+sperumean@users.noreply.github.com>
2024-03-26 18:43:18 -0700

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- // const { signup, error, isLoading } = useSignup();
+  const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     await signup(email, password);
   };
+
   return (
     <div className="cont">
       <form onSubmit={handleSubmit} className="signup">
@@ -28,11 +29,11 @@ const Signup = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
-        {error && <p className="error">Invalid email or password.</p>}
+        {error && <p className="error">{error}</p>}
         <p>
           Already have an account? <Link to="/login">Login</Link>
         </p>
-        <button>Signup</button>
+        <button disabled={isLoading}>Signup</button>
       </form>
     </div>
   );
